49172cb8ab100baa15fb824ed7842fd781b23bdf
meghana123-k <kothakondameghana3@gmail.com>
2026-07-15 17:51:36 +0530

    configure Tailwind CSS with Vite

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
-import { AuthProvider } from "./contexts/AuthContext";
-import AppRoutes from "./routes/AppRoutes";
-
 function App() {
   return (
-    <AuthProvider>
-      <AppRoutes />
-    </AuthProvider>
+    <div className="min-h-screen bg-slate-100 flex items-center justify-center">
+      <div className="rounded-xl bg-white p-10 shadow-lg">
+        <h1 className="text-4xl font-bold text-blue-600">InterviewAI 🚀</h1>
+
+        <p className="mt-4 text-gray-600">
+          Tailwind CSS Successfully Configured
+        </p>
+      </div>
+    </div>
   );
 }
 
